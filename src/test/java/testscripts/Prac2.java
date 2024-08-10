564315a151ace249e4edcaf5b755ead497f924d5
Mohammed <muthahir26.mm@gmail.com>
2024-08-10 18:03:19 +0530

    added additional lines..

--- src/test/java/testscripts/Prac2.java
+++ src/test/java/testscripts/Prac2.java
@@ -3,5 +3,7 @@ package testscripts;
 public class Prac2 {
 	public static void main(String[] args) {
 		System.out.println("Hi");
+		System.out.println("Hello");
+		System.out.println("Bye");
 	}
 }
